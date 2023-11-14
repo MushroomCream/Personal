b3b0f414055c8c694f8d6ae3d975b065a4d12643
Gabryle Antonie Pitogo <148405650+MushroomCream@users.noreply.github.com>
2023-11-15 02:11:57 +0800

    Uploaded Personal Project "Audio Player"
    (Unfinished)

---
+++ Audio Player/AudioPlayer.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+import javax.swing.JOptionPane;
+
+public class AudioPlayer {
+    public static void main(String[]args) {
+        //Naming Instance Is Redundant
+        MyFrame frame = new MyFrame(); 
+       
+    }
+}
